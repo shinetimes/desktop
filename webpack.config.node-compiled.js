24464624a0f1e788d3535c3b39aa43d264a4d33e
Leon Xu <shinetimes@hotmail.com>
2016-05-23 09:14:35 +0800

    更新

---
+++ webpack.config.node-compiled.js
@@ -0,0 +1,14 @@
+// for babel-plugin-webpack-loaders
+require('babel-register');
+const devConfigs = require('./webpack.config.development');
+
+module.exports = {
+  output: {
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders: devConfigs.module.loaders.slice(1) // remove babel-loader
+  }
+};
+
+//# sourceMappingURL=webpack.config.node-compiled.js.map
